ad2e96d673dad0fb1302994c8e64b23a7d6d5be1
Rayane Guemmoud <me@shaynlink.fr>
2020-11-15 20:28:24 +0100

    on error

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const {Cluster} = require('kobu-lib');
 const cluster = new Cluster('src/index.js', require('./config').token);
 
 cluster.spawn();
+
+process.on('rejectionHandled', (err) => console.error(err));
+process.on('uncaughtException', (err) => console.error(err));
+process.on('unhandledRejection', (err) => console.error(err));
